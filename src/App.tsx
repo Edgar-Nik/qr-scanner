e6ee6fd58641088e1386aeb943c382babf612733
Edgar <edgarnikogosian89@gmail.com>
2023-03-13 10:53:33 +0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import QrScanner from 'qr-scanner';
+import { useEffect, useRef, useState } from 'react';
 
-function App() {
+const App = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [result, setResult] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const qrScanner = new QrScanner(
+        videoRef.current,
+        (result) => {
+          setResult(result.data);
+          console.log(result);
+        },
+        {
+          /* your options or returnDetailedScanResult: true if you're not specifying any other options */
+          highlightScanRegion: true,
+          maxScansPerSecond: 10,
+          highlightCodeOutline: true
+        }
+      );
+      qrScanner.start();
+    }
+  }, [videoRef]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video width={800} ref={videoRef}></video>
+
+      <h3>
+        <>Result: {result}</>
+      </h3>
     </div>
   );
-}
+};
 
 export default App;
